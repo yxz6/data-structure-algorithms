2b840f02e950f27e64f05be25c544c0ab4f5890e
Tan <tan@tans-MacBook-Pro.local>
2024-06-09 14:44:50 -0400

    fast commit

--- decimal/minus.go
+++ decimal/minus.go
@@ -2,7 +2,7 @@ package decimal
 
 // Minus two positive integers
 func Minus(a string, b string) string {
-	if len(a) < len(b) || a < b {
+	if len(a) < len(b) || (len(a) == len(b) && a < b) {
 		delta := Minus(b, a)
 		if delta[0] == '-' {
 			return delta[1:]
@@ -17,7 +17,7 @@ func Minus(a string, b string) string {
 	for i >= 0 && j >= 0 {
 		delta := int8(a[i]) - int8(b[j]) - borrow
 		if delta < 0 {
-			delta = -delta
+			delta = 10 + delta
 			borrow = 1
 		}
 		res = append(res, byte(delta)+'0')
@@ -28,8 +28,10 @@ func Minus(a string, b string) string {
 	for i >= 0 {
 		delta := int8(a[i]) - '0' - borrow
 		if delta < 0 {
-			delta = -delta
+			delta = 10 + delta
 			borrow = 1
+		} else {
+			borrow = 0
 		}
 		res = append(res, byte(delta)+'0')
 		i--
@@ -38,19 +40,20 @@ func Minus(a string, b string) string {
 	for j >= 0 {
 		delta := int8(b[j]) - '0' - borrow
 		if delta < 0 {
-			delta = -delta
+			delta = 10 + delta
 			borrow = 1
+		} else {
+			borrow = 0
 		}
 		res = append(res, byte(delta)+'0')
 		j--
 	}
 
-	if borrow > 0 {
-		res = append(res, '-')
-	}
-
 	for left, right := 0, len(res)-1; left < right; left, right = left+1, right-1 {
 		res[left], res[right] = res[right], res[left]
+	}
+	for len(res) > 1 && res[0] == '0' {
+		res = res[1:]
 	}
 	return string(res)
 }
--- decimal/minus_test.go
+++ decimal/minus_test.go
@@ -4,9 +4,13 @@ import "testing"
 
 func TestMinus(t *testing.T) {
 	tests := [][]string{
-		//{"1", "2"},
-		//{"0", "2"},
+		{"1", "2"},
+		{"0", "2"},
 		{"10", "288"},
+		{"100000", "288"},
+		{"289", "288"},
+		{"38", "288"},
+		{"3", "3"},
 	}
 	for _, test := range tests {
 		t.Log(test[0], test[1], Minus(test[0], test[1]))
